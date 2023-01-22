c92baaeccab33fbefa9b5ab47c8c2276a998f8e0
Antonio Prado <antonioeprado18@gmail.com>
2023-01-22 12:23:54 -0300

    feat: create adress from given cep

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -1,16 +1,19 @@
-import { request } from '@/utils/request';
-import { notFoundError, requestError } from '@/errors';
-import addressRepository, { CreateAddressParams } from '@/repositories/address-repository';
-import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
-import { exclude } from '@/utils/prisma-utils';
-import { Address, Enrollment } from '@prisma/client';
+import { request } from "@/utils/request";
+import { notFoundError, requestError } from "@/errors";
+import addressRepository, { CreateAddressParams } from "@/repositories/address-repository";
+import enrollmentRepository, { CreateEnrollmentParams } from "@/repositories/enrollment-repository";
+import { exclude } from "@/utils/prisma-utils";
+import { Address, Enrollment } from "@prisma/client";
+import { ViaCEPAddress } from "@/protocols";
+import { AxiosResponse } from "axios";
 
 async function getAddressFromCEP(cep: string) {
-  const result = await request.get('https://viacep.com.br/ws/37440000/json/');
+  const result = (await request.get(`https://viacep.com.br/ws/${cep}/json/`)) as AxiosResponse<ViaCEPAddress>;
 
   if (!result.data) {
     throw notFoundError();
   }
+  return createAdressWithCEP(result);
 }
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
@@ -22,27 +25,27 @@ async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddr
   const address = getFirstAddress(firstAddress);
 
   return {
-    ...exclude(enrollmentWithAddress, 'userId', 'createdAt', 'updatedAt', 'Address'),
+    ...exclude(enrollmentWithAddress, "userId", "createdAt", "updatedAt", "Address"),
     ...(!!address && { address }),
   };
 }
 
-type GetOneWithAddressByUserIdResult = Omit<Enrollment, 'userId' | 'createdAt' | 'updatedAt'>;
+type GetOneWithAddressByUserIdResult = Omit<Enrollment, "userId" | "createdAt" | "updatedAt">;
 
 function getFirstAddress(firstAddress: Address): GetAddressResult {
   if (!firstAddress) return null;
 
-  return exclude(firstAddress, 'createdAt', 'updatedAt', 'enrollmentId');
+  return exclude(firstAddress, "createdAt", "updatedAt", "enrollmentId");
 }
 
-type GetAddressResult = Omit<Address, 'createdAt' | 'updatedAt' | 'enrollmentId'>;
+type GetAddressResult = Omit<Address, "createdAt" | "updatedAt" | "enrollmentId">;
 
 async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollmentWithAddress) {
-  const enrollment = exclude(params, 'address');
+  const enrollment = exclude(params, "address");
   const address = getAddressForUpsert(params.address);
 
   //TODO - Verificar se o CEP é válido
-  const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, 'userId'));
+  const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, "userId"));
 
   await addressRepository.upsert(newEnrollment.id, address, address);
 }
@@ -51,6 +54,17 @@ function getAddressForUpsert(address: CreateAddressParams) {
   return {
     ...address,
     ...(address?.addressDetail && { addressDetail: address.addressDetail }),
+  };
+}
+
+function createAdressWithCEP(adress: AxiosResponse<ViaCEPAddress>) {
+  const { logradouro, complemento, bairro, localidade, uf }: ViaCEPAddress = adress.data;
+  return {
+    logradouro,
+    complemento,
+    bairro,
+    cidade: localidade,
+    uf,
   };
 }
 
