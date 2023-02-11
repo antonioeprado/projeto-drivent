894052d0b813df8d63bf432e88c40291247d1dd4
Antonio Prado <antonioeprado18@gmail.com>
2023-02-11 15:01:17 -0300

    feat: put bookings controller

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 import { AuthenticatedRequest } from "@/middlewares";
-import { bookRoom, findUserBookings } from "@/services";
+import { bookRoom, changeHotelRooms, findUserBookings } from "@/services";
 import httpStatus from "http-status";
 
 export async function getBookings(req: AuthenticatedRequest, res: Response) {
@@ -11,8 +11,7 @@ export async function getBookings(req: AuthenticatedRequest, res: Response) {
   } catch (error) {
     if (error.name === "NotFoundError") {
       return res.status(httpStatus.NOT_FOUND).send(error.message);
-    }
-    if (error.name === "ForbiddenError") {
+    } else if (error.name === "ForbiddenError") {
       return res.status(httpStatus.FORBIDDEN).send(error.message);
     }
   }
@@ -29,6 +28,23 @@ export async function postBooking(req: AuthenticatedRequest, res: Response) {
       return res.status(httpStatus.FORBIDDEN).send(error.message);
     } else if (error.name === "NotFoundError") {
       return res.status(httpStatus.NOT_FOUND).send(error.message);
+    }
+    return res.sendStatus(httpStatus.FORBIDDEN);
+  }
+}
+
+export async function putBooking(req: AuthenticatedRequest, res: Response) {
+  try {
+    const { bookingId } = req.params as Record<string, string>;
+    const { roomId } = req.body as Record<string, string>;
+    const { userId } = req;
+    const newBookingId = await changeHotelRooms(userId, Number(bookingId), Number(roomId));
+    res.status(httpStatus.OK).send({ bookingId: newBookingId.id });
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.status(httpStatus.NOT_FOUND).send(error.message);
+    } else if (error.name === "ForbiddenError") {
+      return res.status(httpStatus.FORBIDDEN).send(error.message);
     }
     return res.sendStatus(httpStatus.FORBIDDEN);
   }
