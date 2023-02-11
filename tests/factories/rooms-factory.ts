0064dde7ec87a9715f9a3cc0d0c749039dbd9615
Antonio Prado <antonioeprado18@gmail.com>
2023-02-11 13:37:33 -0300

    feat: create a booked room

--- tests/factories/rooms-factory.ts
+++ tests/factories/rooms-factory.ts
@@ -1,12 +1,24 @@
+import { prisma } from "@/config";
 import faker from "@faker-js/faker";
 
-export function createMultipleRooms() {
+export function createMultipleRooms(capacity?: number) {
   const roomsArr = [];
+  const cap = capacity || faker.datatype.number({ min: 0, max: 3 });
   for (let i = 0; i < 5; i++) {
     roomsArr.push({
       name: faker.name.findName(),
-      capacity: faker.datatype.number({ max: 4 }),
+      capacity: cap,
     });
   }
   return roomsArr;
+}
+
+export function createBookedRoom(hotelId: number) {
+  return prisma.room.create({
+    data: {
+      name: faker.name.findName(),
+      capacity: 0,
+      hotelId,
+    },
+  });
 }
