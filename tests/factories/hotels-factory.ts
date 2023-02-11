e615b93297b41b94645424ec3feba0e8ed2c0e19
Antonio Prado <antonioeprado18@gmail.com>
2023-02-11 13:37:13 -0300

    refactor: optional parameter to create room with given capacity

--- tests/factories/hotels-factory.ts
+++ tests/factories/hotels-factory.ts
@@ -2,16 +2,17 @@ import { prisma } from "@/config";
 import faker from "@faker-js/faker";
 import { createMultipleRooms } from "./rooms-factory";
 
-export function createHotel() {
+export function createHotel(roomCap?: number) {
   return prisma.hotel.create({
     data: {
       name: faker.name.findName(),
       image: faker.image.imageUrl(),
       Rooms: {
         createMany: {
-          data: createMultipleRooms(),
+          data: createMultipleRooms(roomCap),
         },
       },
     },
+    include: { Rooms: true },
   });
 }
