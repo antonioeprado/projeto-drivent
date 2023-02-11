d65aeea3bb99a1dbf015b793d81c7fcb4a8ff568
Antonio Prado <antonioeprado18@gmail.com>
2023-02-11 14:19:09 -0300

    feat: get bookings controller

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -1,7 +1,22 @@
 import { Response } from "express";
 import { AuthenticatedRequest } from "@/middlewares";
-import { bookRoom } from "@/services";
+import { bookRoom, findUserBookings } from "@/services";
 import httpStatus from "http-status";
+
+export async function getBookings(req: AuthenticatedRequest, res: Response) {
+  try {
+    const { userId } = req;
+    const userBookings = await findUserBookings(userId);
+    res.status(httpStatus.OK).send(userBookings);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.status(httpStatus.NOT_FOUND).send(error.message);
+    }
+    if (error.name === "ForbiddenError") {
+      return res.status(httpStatus.FORBIDDEN).send(error.message);
+    }
+  }
+}
 
 export async function postBooking(req: AuthenticatedRequest, res: Response) {
   try {
